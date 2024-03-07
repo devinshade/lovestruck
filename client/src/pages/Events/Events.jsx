dc3209a19fecbc07d994f1f90d55fd170062f69f
devinshade <devindanielle93@gmail.com>
2024-03-06 19:52:41 -0700

    added 'create event' submit button and revised preferred name to seperate first/last name

--- client/src/pages/Events/Events.jsx
+++ client/src/pages/Events/Events.jsx
@@ -4,8 +4,8 @@ import Form from '../../components/eventForm';
 
 const Events = () => {
   return (<>
-    <div style={{backgroundColor: '#E6EEF1', height: '100%'}}>Create an Event</div>
-    <Form />;
+    <div style={{backgroundColor: '#E6EEF1', height: '100%'}}>Events</div>
+    <Form />
     </>)
 }
 
