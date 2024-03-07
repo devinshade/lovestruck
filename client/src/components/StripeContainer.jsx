fc3aeac1a4584402233fa0012903fa729557cc4e
Deboh12 <deborah.alexander12@yahoo.com>
2024-03-06 20:03:46 -0600

    import from DonationForm component

--- client/src/components/StripeContainer.jsx
+++ client/src/components/StripeContainer.jsx
@@ -1,3 +1,6 @@
 import React from 'react';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
+import DonationForm from './DonationForm';
+
+const stripePromise = loadStripe('YOUR_STRIPE_PUBLISHABLE_KEY');
