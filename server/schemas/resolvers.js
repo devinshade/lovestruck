e6b6da2bc0efc977ba6670d405f75d6b8a3d51fa
laude-noctis <lizzybumpers11@gmail.com>
2024-03-06 18:41:20 -0600

    mutation: addUser

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,8 +4,13 @@ const { signToken, AuthenticationError } = require('../utils/auth');
 const resolvers = {
     Query: {
 
-    }
+    },
     Mutation: {
-        
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user }
+        },
     }
 }
