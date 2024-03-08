c77d2ec34b7945bdbed97b10c1a17cf790ac60cd
devinshade <devindanielle93@gmail.com>
2024-03-07 21:03:22 -0700

    updated form styling

--- client/src/components/eventForm/index.jsx
+++ client/src/components/eventForm/index.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
-// import './style.css';
+import './style.css';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
+import Card from 'react-bootstrap/Card';
 
 function EventForm() {
     // Create state variables for the fields in the form
@@ -44,81 +45,83 @@ function EventForm() {
     return (
         <div className="container text-center">
             <h1>Welcome {firstName}!</h1>
-            <Form onSubmit={handleFormSubmit}>
-                <Row className="mb-3">
-                    <Form.Group as={Col} controlId="formGridFirstName">
-                    <Form.Label>First Name</Form.Label>
-                    <Form.Control
-                        value={firstName}
-                        name="firstName"
+            <Card className="container text-center" style={{ width: '44rem' }}>
+                <Form onSubmit={handleFormSubmit}>
+                    <Row className="mb-3">
+                        <Form.Group as={Col} controlId="formGridFirstName">
+                        <Form.Label>First Name</Form.Label>
+                        <Form.Control
+                            value={firstName}
+                            name="firstName"
+                            onChange={handleInputChange}
+                            type="text"
+                            placeholder='First Name'
+                        />
+                        </Form.Group>
+            
+                        <Form.Group as={Col} controlId="formGridLastName">
+                        <Form.Label>Last Name</Form.Label>
+                        <Form.Control
+                            value={lastName}
+                            name="lastName"
+                            onChange={handleInputChange}
+                            type="text"
+                            placeholder='Last Name'
+                        />
+                        </Form.Group>
+                    </Row>
+            
+                    <Form.Group className="mb-3" controlId="formGridEventName">
+                        <Form.Label>Event Name</Form.Label>
+                        <Form.Control
+                        value={eventId}
+                        name='event'
                         onChange={handleInputChange}
-                        type="text"
-                        placeholder='First Name'
-                    />
+                        type='text'
+                        placeholder='Event Name'
+                        />
                     </Form.Group>
-        
-                    <Form.Group as={Col} controlId="formGridLastName">
-                    <Form.Label>Last Name</Form.Label>
-                    <Form.Control
-                        value={lastName}
-                        name="lastName"
+            
+                    <Form.Group className="mb-3" controlId="formGridLocation">
+                        <Form.Label>Location</Form.Label>
+                        <Form.Control
+                        value={location}
+                        name='location'
                         onChange={handleInputChange}
-                        type="text"
-                        placeholder='Last Name'
-                    />
+                        type='text'
+                        placeholder='Location of Event'
+                        />
                     </Form.Group>
-                </Row>
-        
-                <Form.Group className="mb-3" controlId="formGridEventName">
-                    <Form.Label>Event Name</Form.Label>
-                    <Form.Control
-                    value={eventId}
-                    name='event'
-                    onChange={handleInputChange}
-                    type='text'
-                    placeholder='Event Name'
-                    />
-                </Form.Group>
-        
-                <Form.Group className="mb-3" controlId="formGridLocation">
-                    <Form.Label>Location</Form.Label>
-                    <Form.Control
-                    value={location}
-                    name='location'
-                    onChange={handleInputChange}
-                    type='text'
-                    placeholder='Location of Event'
-                    />
-                </Form.Group>
-        
-                <Row className="mb-3">
-                    <Form.Group as={Col} controlId="formGridDate">
-                    <Form.Label>Date</Form.Label>
-                    <Form.Control
-                        value={date}
-                        name='date'
-                        onChange={handleInputChange}
-                        type='date'
-                        placeholder='Date of Event'
-                    />
-                    </Form.Group>
-        
-                    <Form.Group as={Col} controlId="formGridContactInfo">
-                    <Form.Label>Contact Info</Form.Label>
-                    <Form.Control
-                        value={contactInfo}
-                        name='contactInfo'
-                        onChange={handleInputChange}
-                        type='number'
-                        placeholder='Phone number'
-                    />
-                    </Form.Group>
-                </Row>
-        
-                <Button variant="primary" type="submit">
-                    Create Event
-                </Button>
-            </Form>
+            
+                    <Row className="mb-3">
+                        <Form.Group as={Col} controlId="formGridDate">
+                        <Form.Label>Date</Form.Label>
+                        <Form.Control
+                            value={date}
+                            name='date'
+                            onChange={handleInputChange}
+                            type='date'
+                            placeholder='Date of Event'
+                        />
+                        </Form.Group>
+            
+                        <Form.Group as={Col} controlId="formGridContactInfo">
+                        <Form.Label>Contact Info</Form.Label>
+                        <Form.Control
+                            value={contactInfo}
+                            name='contactInfo'
+                            onChange={handleInputChange}
+                            type='number'
+                            placeholder='Phone number'
+                        />
+                        </Form.Group>
+                    </Row>
+            
+                    <Button variant="primary" type="submit">
+                        Create Event
+                    </Button>
+                </Form>
+            </Card>
         </div>
     )
 }
