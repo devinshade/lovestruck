e9a310940dcf5a8f27c4a053e27b7839974d5169
Deboh12 <deborah.alexander12@yahoo.com>
2024-03-07 18:53:05 -0600

    creating a payment method

--- client/src/components/DonationForm.jsx
+++ client/src/components/DonationForm.jsx
@@ -2,7 +2,33 @@ import React from 'react';
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 
 const DonationForm = () => {
-  const stripe = useStripe();
-  const elements = useElements();
+    const stripe = useStripe();
+    const elements = useElements();
+  
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      const { error, paymentMethod } = await stripe.createPaymentMethod({
+        type: 'card',
+        card: elements.getElement(CardElement),
+      });
+  
+      if (!error) {
+        // Send the paymentMethod.id to your server (see step 3)
+        console.log(paymentMethod);
+        // Handle the payment confirmation and display a success message
+      } else {
+        // Handle errors
+        console.log(error);
+      }
+    };
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <CardElement />
+        <button type="submit" disabled={!stripe}>
+          Donate
+        </button>
+      </form>
+    );
   };
 
