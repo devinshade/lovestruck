4d6248ee78294ac0aaa77342aa5b726804becd8e
Deboh12 <deborah.alexander12@yahoo.com>
2024-03-07 18:49:04 -0600

    initializing Stripe hooks

--- client/src/components/DonationForm.jsx
+++ client/src/components/DonationForm.jsx
@@ -1,3 +1,8 @@
 import React from 'react';
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 
+const DonationForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  };
+
