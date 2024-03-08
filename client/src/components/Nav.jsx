9c426b0c7e07c61d053235fb75ada88aaa2c55bb
TylerFruik <tylerfruik@gmail.com>
2024-03-07 18:40:33 -0600

    Added in CSS from P2 as starter work. General changes made everywhere.

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -5,15 +5,41 @@ import './Nav.css';
 
 const Nav = () => {
   return (
-    <nav>
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            |
-            <li><Link to="/events">Events</Link></li>
-            |
-            <li><Link to="/profile">My Profile</Link></li>
-        </ul>
-    </nav>
+    <header>
+      <h1>
+        <a href="/">💗 LoveStruck 💗</a>
+      </h1>
+      <nav>
+        <a href="/events">
+          <button className="custom-btn header-btns">Events</button>
+        </a>
+        |
+        <a href="/">
+          <button className="custom-btn header-btns">Calendar</button>
+        </a>
+        |
+        <a href="/">
+          <button className="custom-btn header-btns">My Wedding</button>
+        </a>
+        |
+        <a href="/">
+          <button className="custom-btn header-btns">Friends</button>
+        </a>
+        |
+        <a href="/login">
+          <button className="custom-btn header-btns">Login</button>
+        </a>
+      </nav>
+    </header>
+    // <nav>
+    //     <ul>
+    //         <li><Link to="/">Home</Link></li>
+    //         |
+    //         <li><Link to="/events">Events</Link></li>
+    //         |
+    //         <li><Link to="/profile">My Profile</Link></li>
+    //     </ul>
+    // </nav>
   )
 }
 
