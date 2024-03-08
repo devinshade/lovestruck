02a50131e7f60fb07ae457a16d6aad08f929817c
Deboh12 <deborah.alexander12@yahoo.com>
2024-03-07 19:41:30 -0600

    forgot to add quotes around my stripe key hehe

--- client/src/components/StripeContainer.jsx
+++ client/src/components/StripeContainer.jsx
@@ -3,7 +3,7 @@ import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import DonationForm from './DonationForm';
 
-const stripePromise = loadStripe(pk_test_51OrsCMHiPcL6rzSyxeBOkMM8uBSH1OYDboOpESNDd504L2gB1VBaHw3yMJ9nEQmX9NZuOQLhJnkXNN3s5WbMWo5p007WtoYLOd);
+const stripePromise = loadStripe('pk_test_51OrsCMHiPcL6rzSyxeBOkMM8uBSH1OYDboOpESNDd504L2gB1VBaHw3yMJ9nEQmX9NZuOQLhJnkXNN3s5WbMWo5p007WtoYLOd');
 
 const StripeContainer = () => {
     return (
