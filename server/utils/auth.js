0a51544d335d5235db25f2ffc6c04012e47c1c87
laude-noctis <lizzybumpers11@gmail.com>
2024-03-13 16:46:53 -0500

    I think the images will render now maybe hopefully

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,13 +1,16 @@
+const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
-
 // set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
   authMiddleware: function ({req}) {
-    // allows token to be sent via  req.query or headers
     let token = req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
