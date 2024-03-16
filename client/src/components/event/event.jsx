732d88526c0cf0f7c92163f3566f863cda5e8120
laude-noctis <lizzybumpers11@gmail.com>
2024-03-16 07:34:33 -0500

    got the update/delete buttons to show if the user is the same as the event creator

--- client/src/components/event/event.jsx
+++ client/src/components/event/event.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import { useMutation, useQuery } from '@apollo/client';
 import { DELETE_EVENT } from '../../utils/mutations';
-import { QUERY_EVENTS } from '../../utils/queries';
-import Auth from '../../utils/auth'
+import { QUERY_EVENTS, QUERY_ME } from '../../utils/queries';
 
 const Event = ({ event }) => {
     const [deleteEvent] = useMutation(DELETE_EVENT)
+    const { data } = useQuery(QUERY_ME);
+    const [userData, setUserData] = useState({});
     const { refetch: refetchEvents } = useQuery(QUERY_EVENTS);
-    
+
+    useEffect(() => {
+        if (data && data.me) {
+            setUserData(data.me);
+        }
+    }, [data]);
+
     const handleDelete = async (eventId) => {
         try {
             await deleteEvent({
@@ -22,36 +29,43 @@ const Event = ({ event }) => {
             console.error(err)
         }
     }
+    
+    const userId = userData._id
 
     const handleRSVP = () => {
         window.location.href = `./rsvp/${event._id}`
     }
 
-    console.log("the event:", event)
-
     return (
-    <div className='m-5 row'>
-        <div className="custom-border var-bg-blue2 col-md-8 flex-wrap mx-auto d-flex p-4">
-            <div className='mx-5 var-text-light custom-text-lg col-8'>
-                <h3 className='love-struck p-3 custom-underline custom-text-xxl'>{event.title}</h3>
-                <p><strong>Host(s):</strong> {event.firstName} {event.lastName}</p>
-                <p><strong>Date:</strong> {event.date}</p>
-                <p><strong>Description:</strong> {event.description}</p>
-                <p><strong>Location:</strong> {event.location}</p>
-            </div>
-            <div className='mx-auto my-auto justify'>
-                    <button className="custom-btn p-4 custom-text-md" onClick={() => handleDelete(event._id)}>
-                         Delete
-                    </button>
-                    <button className="custom-btn p-4 custom-text-md" onClick={handleRSVP}>
-                        RSVP today!
-                    </button>
-                    <Link to={{ pathname: `/updateEvents/${event._id}`, eventInfo: event }}>
-                        <button className="custom-btn p-4 custom-text-md">Update</button>
-                    </Link>
+        <div className='m-5 row'>
+            <div className="custom-border var-bg-blue2 col-md-8 flex-wrap mx-auto d-flex p-4">
+                <div className='mx-5 var-text-light custom-text-lg col-8'>
+                    <h3 className='love-struck p-3 custom-underline custom-text-xxl'>{event.title}</h3>
+                    <p><strong>Host(s):</strong> {event.firstName} {event.lastName}</p>
+                    <p><strong>Date:</strong> {event.date}</p>
+                    <p><strong>Description:</strong> {event.description}</p>
+                    <p><strong>Location:</strong> {event.location}</p>
+                </div>
+                <div className='mx-auto my-auto justify'>
+                    {userId === event.creator._id ? (
+                        <>
+                            <button className="custom-btn p-4 custom-text-md" onClick={() => handleDelete(event._id)}>
+                                Delete
+                            </button>
+                            <Link to={{ pathname: `/updateEvents/${event._id}`, eventInfo: event }}>
+                                <button className="custom-btn p-4 custom-text-md">Update</button>
+                            </Link>
+                        </>
+                    ) : (
+                        <>
+                            <button className="custom-btn p-4 custom-text-md" onClick={handleRSVP}>
+                                RSVP today!
+                            </button>
+                        </>
+                    )}
+                </div>
             </div>
         </div>
-    </div>
     );
 };
 
