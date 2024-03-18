a940814aade1d5ade3083cd5172cf5ef6b4a241c
TylerFruik <tylerfruik@gmail.com>
2024-03-18 16:30:28 -0500

    Styled the RSVP page to match the rest of the application

--- client/src/pages/RSVP/rsvp.jsx
+++ client/src/pages/RSVP/rsvp.jsx
@@ -40,27 +40,48 @@ const RSVPForm = () => {
     }
 
     return (
-        <div>
-        {/* Here Tyler */}
-            <Form onSubmit={handleRSVP}>
-                <Form.Control
-                    value={rsvpState.firstName}
-                    name="firstName"
-                    onChange={handleInputChange}
-                    type="text"
-                    placeholder='First Name'
-                />
-                <Form.Control
-                    value={rsvpState.lastName}
-                    name="lastName"
-                    onChange={handleInputChange}
-                    type="text"
-                    placeholder='Last Name'
-                />
-                <button type='submit'>
-                    RSVP!
-                </button>
-            </Form>
+        <div className='fullPage container mx-auto pt-5'>
+            <div className='row justify-center'>
+                <div className="col-3 text-center" id="img-container">
+                    <img className="login-img custom-border" src="/src/assets/images/Photo1.png" alt="A couple getting married"/>
+                    <img className="login-img custom-border" src="/src/assets/images/Photo3.png" alt="A couple getting married"/>
+                </div>
+                <div className='col-5 var-bg-blue2 custom-border var-text-light'>
+                    <div className='m-5'>
+                        <h1 className='text-center custom-underline'>RSVP Below!</h1>
+                        <Form onSubmit={handleRSVP}>
+                            <Form.Control
+                                value={rsvpState.firstName}
+                                name="firstName"
+                                onChange={handleInputChange}
+                                type="text"
+                                placeholder='First Name'
+                                className='my-3'
+                            />
+                            <Form.Control
+                                value={rsvpState.lastName}
+                                name="lastName"
+                                onChange={handleInputChange}
+                                type="text"
+                                placeholder='Last Name'
+                                className='my-3'
+                            />
+                            <div className='text-center'>
+                                <button type='submit'
+                                    className="custom-btn p-3 px-5 custom-text-md mt-5"
+                                    style={{ cursor: 'pointer' }}
+                                    >
+                                    RSVP!
+                                </button>
+                            </div>
+                        </Form>
+                    </div>
+                </div>
+                <div className="col-3 text-center" id="img-container">
+                    <img className="login-img custom-border" src="/src/assets/images/Photo6.png" alt="A couple getting married"/>
+                    <img className="login-img custom-border" src="/src/assets/images/Photo2.png" alt="A couple getting married"/>
+                </div>
+            </div>
         </div>
     )
 }
